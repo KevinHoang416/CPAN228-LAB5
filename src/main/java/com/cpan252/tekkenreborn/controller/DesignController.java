643acbfe1214adb05db683ff7dd5c3a59fcf9a64
Kevin Hoang <kevinhoang@Kevins-MacBook-Air.local>
2023-02-25 18:13:55 -0500

    first commit

---
+++ src/main/java/com/cpan252/tekkenreborn/controller/DesignController.java
@@ -0,0 +1,58 @@
+package com.cpan252.tekkenreborn.controller;
+
+import java.util.EnumSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cpan252.tekkenreborn.model.Fighter;
+import com.cpan252.tekkenreborn.model.Fighter.Anime;
+import com.cpan252.tekkenreborn.repository.FighterRepository;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/design")
+public class DesignController {
+
+    @Autowired
+    private FighterRepository fighterRepository;
+
+    @GetMapping
+    public String design() {
+        return "design";
+    }
+
+    @ModelAttribute
+    public void animes(Model model) {
+        var animes = EnumSet.allOf(Anime.class);
+        model.addAttribute("animes", animes);
+        log.info("animes converted to string:  {}", animes);
+    }
+
+    @ModelAttribute
+    public Fighter fighter() {
+        return Fighter
+                .builder()
+                .build();
+    }
+
+    @PostMapping
+    public String processFighterAddition(@Valid Fighter fighter, BindingResult result) {
+        if (result.hasErrors()) {
+            return "design";
+        }
+        log.info("Processing fighter: {}", fighter);
+        fighterRepository.save(fighter);
+        return "redirect:/fighterlist";
+    }
+
+}
